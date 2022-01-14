8d46eb824a445db5bf0e51fac2a5919e8fde92ef
Andres Felipe Mendoza <ibaguetolima@hotmail.com>
2022-01-14 17:58:05 -0500

    first commit

---
+++ valores.js
@@ -0,0 +1,10 @@
+40 /* valor tipo numero */
+"Texto" /* valor tipo string */
+true /* valor tipo booleano */
+false /* valor tipo booleano */
+null /* valor tipo nulo valores placeholder*/
+undefined /* valor tipo indefinido valores placeholder*/
+[1,2,3,4,5] /* valor tipo array */
+{nombre: "Juan", edad: 30} /* valor tipo objeto */
+typeof 40/* para saber el tipo de dato es como el dtype de python */
+
---
+++ varibales.js
@@ -0,0 +1,14 @@
+/* Para guardar los valores en memoria */
+var nombre = "Juan"; /* var resperenta que es una variable */
+/* 
+Hay 2 estados de una variable:
+ */
+//declarar -> esta es la forma de hacer comentarios
+var edad; /* aca se esta declarando la variable */
+
+// inicializar
+edad = 30; /* aca se esta inicializando la variable */
+
+/* varibles mas complejas */
+var elementos = ["Juan", "Pedro", "Maria"];
+
